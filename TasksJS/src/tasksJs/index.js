ab1b4482c39a511794964ad6b2ea1fede1cd1ac3
eleonora <nebrudko18@gmail.com>
2023-06-01 17:32:46 +0300

    Smallest Common Multiple

--- TasksJS/src/tasksJs/index.js
+++ TasksJS/src/tasksJs/index.js
@@ -14,7 +14,31 @@ function sumPrimes(num) {
   return primes.reduce((acc, curr) => acc + curr);
 }
 
-console.log(sumPrimes(10)); //17  
+sumPrimes(10); //17  
 
 
 
+//Smallest Common Multiple
+function smallestCommons(arr) {
+  var range = [];
+  let sorted = arr.sort((a, b) => a - b)
+  for (let i = sorted[0]; i <= sorted[1]; i++) {
+    range.push(i);
+  }
+
+  var lcm = range[0];
+  for (let i = 1; i < range.length; i++) {
+    let GCD = gcd(lcm, range[i]);
+    lcm = (lcm * range[i]) / GCD;
+  }
+  return lcm;
+
+  function gcd(x, y) {
+    if (y === 0)
+      return x;
+    else
+      return gcd(y, x % y);
+  }
+}
+
+console.log(smallestCommons([1, 5]));
