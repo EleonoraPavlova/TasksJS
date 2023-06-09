89955dcaeec3be38edb46204423012d53f7cac46
eleonora <nebrudko18@gmail.com>
2023-06-09 15:04:48 +0300

    Cash Register

--- TasksJS/src/tasksJs/index.js
+++ TasksJS/src/tasksJs/index.js
@@ -261,4 +261,40 @@ function telephoneCheck(str) {
   return re.test(str)
 }
 
-console.log(telephoneCheck("11 555-555-5555"));
+telephoneCheck("11 555-555-5555");
+
+
+
+//Cash Register
+function checkCashRegister(price, cash, cid) {
+  let change = (cash * 100) - (price * 100);
+  let count = 0
+  let cidTotal = cid.map((el) => count += el[1] * 100);
+  if (change > count) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] }
+  } else if (change === count) {
+    return { status: "CLOSED", change: cid }
+  } else {
+    let answer = []
+    cid = cid.reverse();
+    let moneyUnits = { "ONE HUNDRED": 10000, "TWENTY": 2000, "TEN": 1000, "FIVE": 500, "ONE": 100, "QUARTER": 25, "DIME": 10, "NICKEL": 5, "PENNY": 1 }
+    for (let elem of cid) {
+      let holder = [elem[0], 0] //как папка значений ['ONE HUNDRED', 0]
+      elem[1] = elem[1] * 100
+      while (change >= moneyUnits[elem[0]] && elem[1] > 0) {
+        change -= moneyUnits[elem[0]]
+        elem[1] -= moneyUnits[elem[0]]
+        holder[1] += moneyUnits[elem[0]] / 100
+      }
+      if (holder[1] > 0) {
+        answer.push(holder)
+      }
+    }
+    if (change > 0) {
+      return { status: "INSUFFICIENT_FUNDS", change: [] }
+    }
+    return { status: "OPEN", change: answer };
+  }
+
+}
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
