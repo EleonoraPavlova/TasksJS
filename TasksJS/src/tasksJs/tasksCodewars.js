8fd8d98e2dc99d45b25f6e8b7553142f13e1b970
eleonora <nebrudko18@gmail.com>
2023-06-12 16:11:45 +0300

    Calculate the function f(x) for a simple linear

--- TasksJS/src/tasksJs/tasksCodewars.js
+++ TasksJS/src/tasksJs/tasksCodewars.js
@@ -33,7 +33,41 @@ largestArrangement([8, 6, 590, 70]) // 8706590
 
 
 //Small enough?
-function smallEnough(arr, limit) {
-  return arr.every((el) => el <= limit) ? true : false
+// function smallEnough(arr, limit) {
+//   return arr.every((el) => el <= limit) ? true : false
+// }
+// smallEnough(([78, 117, 110, 99, 104, 117, 107, 115], 100)) //false
+
+
+
+//Calculate the function f(x) for a simple linear sequence (Easy)
+function getFunction(sequence) {
+  let add = sequence[0], mul = sequence[1] - add, linear = sequence.every(function (v, i) { return v == i * mul + add });
+  return linear ? "f(x) = " + (mul ? (mul == 1 ? '' : mul == -1 ? "-" : mul) + "x" + (!add ? "" : (add > 0 ? " + " : " - ") + Math.abs(add)) : add) : "Non-linear sequence"
 }
-smallEnough(([78, 117, 110, 99, 104, 117, 107, 115], 100)) //false
+
+console.log(getFunction([1, 4, 7, 10, 13]))  //"f(x) = x", "Nope! Try again."
+//an = 2n(cтепень2) + 1 - n число которое проверяем на послед
+
+
+// let seq = [];
+
+// for (let i = 0; i < sequence.length; i++) {
+//   if (sequence[i] < sequence[i + 1]) {
+//     let x = sequence[i + 1] - sequence[i]
+//     seq.push(x)
+//     console.log(x)
+//   }
+// }
+// let res = seq.every((el, index) => el[0] === el[index])
+// if (res === true) {
+//   if (seq[0] === 1) {
+//     return 'f(x) = x'
+//   } else if (sequence[0] === 1) {
+//     return `f(x) = ${seq[0]}` + 'x + 1'
+//   } else if (seq[0] > 2) {
+//     return `f(x) = ${seq[0]}` + 'x'
+//   }
+// } else {
+//   return "Non - linear sequence";
+// }
