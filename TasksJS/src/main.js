c3cee78505df5a9c2e71d284d0213e91b3c621a5
eleonora <nebrudko18@gmail.com>
2023-06-15 15:03:21 +0300

    done Survey Form

--- TasksJS/src/main.js
+++ TasksJS/src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-// import './tasksJs/index'
-import './freeCodeCampForm/index.html'
+import '../style.css'
+import '../index.html'
+// import './freeCodeCampForm/index.html'
 
 import { createApp } from 'vue'
 import App from './App.vue'
